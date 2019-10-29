8be9d0ce8d816b6a678f6127a61a94ebd42e49b6
Evans Ibok <evansibok@gmail.com>
2019-10-29 15:26:28 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+
+  return (
+    <Layout>
+      <h1> Hi, I am Evans</h1>
+
+      <h3>I love designing websites and building digital products!</h3>
+
+    </Layout>
+  )
+}
+
+export default AboutPage;
