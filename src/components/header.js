6b1da86cd06158bb8f00db1c43afe73c7f2a8b58
Evans Ibok <evansibok@gmail.com>
2019-11-06 04:33:23 +0100

    edited the styling for the header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,19 +4,33 @@ import React from "react"
 
 const Header = () => {
   return (
-    <header>
-      <div>
-          <Link to="/">
+    <header
+      style={{
+        maxWidth: `100vw`,
+      }}
+    >
+      <div
+        className="headerContentContainer"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 1080,
+          padding: `0px 1.0875rem`,
+          paddingTop: 0,
+        }}
+      >
+        <div>
+          <Link to="/" style={{ margin: 0, padding: 0 }}>
             <img
               src="/static/e152b5cfbba58a414f35c976307c3c1d/25835/evnsvec.png"
               alt="brand logo"
             />
           </Link>
-      </div>
-      <div>
-        <Link to="/about-me">About Me</Link>
-        <Link to="/projects">Projects</Link>
-        <button>View Resume</button>
+        </div>
+        <div>
+          <Link to="/about-me">About Me</Link>
+          <Link to="/projects">Projects</Link>
+          <button>View Resume</button>
+        </div>
       </div>
     </header>
   )
