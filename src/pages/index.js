8afac4fd36db9c570eb6ceafaadd27af80cb85b3
Evans Ibok <evansibok@gmail.com>
2019-11-25 18:34:13 +0100

    added the homepage content and external links

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,40 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1><span>H</span>ello, I <span>a</span>m <span>E</span>vans</h1>
-    <h5>A Nigerian born web developer with incredible love for UI/UX Design.</h5>
-  </Layout>
+	<Layout>
+		<SEO title="Home" />
+
+		<div>
+			<h1 className="h1HomeCon">
+				<span className="letCol">H</span>ello, I <span className="letCol">a</span>m <span className="letCol">E</span>vans
+			</h1>
+
+			<h4 className="h4HomeCon">A Nigerian born web developer with incredible love for UI/UX Design.</h4>
+
+			<ul>
+				<a href="https://twitter.com/evansibok" target="_blank"
+					rel="noopener noreferrer">
+					<li>Twitter</li>
+				</a>
+				<a href="https://twitter.com/evansibok" target="_blank"
+					rel="noopener noreferrer">
+					<li>LinkedIn</li>
+				</a>
+				<a href="https://twitter.com/evansibok" target="_blank"
+					rel="noopener noreferrer">
+					<li>Podcast</li>
+				</a>
+				<a href="https://twitter.com/evansibok" target="_blank"
+					rel="noopener noreferrer">
+					<li>Github</li>
+				</a>
+				<a href="https://twitter.com/evansibok" target="_blank"
+					rel="noopener noreferrer">
+					<li>Email</li>
+				</a>
+			</ul>
+		</div>
+	</Layout>
 )
 
 export default IndexPage
