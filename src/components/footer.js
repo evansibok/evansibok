108880359acecdafc174e8dc7b0322faf3e40924
Evans Ibok <evansibok@gmail.com>
2019-11-25 17:52:26 +0100

    added a homepage sass file to style homepage

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer>
       <p>
 
-        &copy; {new Date().getFullYear()}. Designed &amp; Developed with <i class="em-svg em-hearts"></i> by {` `}
+        &copy; {new Date().getFullYear()}. Designed &amp; Developed with <i className="em-svg em-hearts"></i> by {` `}
         <span className="letCol">
           <Link to="/">E V \ N S</Link>
         </span>
