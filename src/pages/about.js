6875ac5a960ced288f2f8f63071ed7e0cdc0ed9a
Evans Ibok <evansibok@gmail.com>
2019-11-26 14:45:55 +0100

    added the content layout for the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,11 +10,16 @@ const AboutPage = () => {
       <SEO title="About" />
       <PageLabel />
       <div className="aboutCon">
-        <div>
+        <div className="leftCon">
           <p>I design and build websites using weell crafted human-centered design approaches, modern tools and a combination of both sides of my brain.
           </p>
           <p>When I'm not designing beautiful user experiences or writing functional code for websites, I listen to music, watch fantasy shows, drink coffee, read a book or write a poem.
           </p>
+        </div>
+
+        <div className="rightCon">
+          <p>images</p>
+          
         </div>
 
       </div>
