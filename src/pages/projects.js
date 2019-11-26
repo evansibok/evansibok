0a8a719bd8ddbd9c131c801b72a54c7772cb8a04
Evans Ibok <evansibok@gmail.com>
2019-11-26 17:56:59 +0100

    modified view resume button

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
 import { PageLabel } from "../components/page-label"
 import { SocialLinks } from "../components/social-links"
 
