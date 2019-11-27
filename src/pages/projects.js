cd36caf4a336fdd544d2fe246269ad3caeff8648
Evans Ibok <evansibok@gmail.com>
2019-11-27 07:45:29 +0100

    structure created for the project card

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,6 +12,38 @@ const ProjectsPage = () => {
 			<SEO title="Projects" />
 			<PageLabel name="Projects" />
 			<div className="projectCon">
+				<div>
+					<div className="cardHead">
+						<h4>Pintereach</h4>
+
+						<ul>
+							<a>
+								<li>Website</li>
+							</a>
+							<a>
+								<li>Github</li>
+							</a>
+						</ul>
+					</div>
+
+					<div className="cardBody">
+						<div className="leftCon">
+							<p>
+								Fort NG is a fashion company based in Nigeria. Customers place orders by viewing items on instagram and sending messages. They wanted an E-Commerce website for users to purchase items from as well as a website where potential customers would get the latest news.<br /><br />
+
+								The E-Commerce website was built from scratch using Laravel. An admin portal was also built to enable admins manage news, products, orders as well as website content.
+							</p>
+							<div className="projectIcons">
+								<img src="" alt="html" />
+								<img src="" alt="css" />
+								<img src="" alt="js" />
+							</div>
+						</div>
+						<div className="imageCon">
+							<img src="" alt="pintereach screenshot" />
+						</div>
+					</div>
+				</div>
 
 			</div>
 			<SocialLinks />
