5ec9ec1c11a288b42d27ce12863fcd03160ff756
Evans Ibok <evansibok@gmail.com>
2019-12-04 17:03:17 +0100

    used try and catch to remove the window undefined error in build

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -3,14 +3,26 @@ import { useState } from "react"
 export const useLocalStorage = (key, initialValue) => {
 
 	const [storedValue, setStoredValue] = useState(() => {
-		const currentLocalValue = localStorage.getItem(key)
 
-		return currentLocalValue ? JSON.parse(currentLocalValue) : initialValue
+		try {
+			const currentLocalValue = window.localStorage.getItem(key)
+
+			return currentLocalValue ? JSON.parse(currentLocalValue) : initialValue
+		} catch (error) {
+			console.log(error);
+			return initialValue;
+		}
 	})
 
 	const setValue = value => {
-		setStoredValue(value)
-		localStorage.setItem(key, JSON.stringify(value))
+
+		try {
+			setStoredValue(value)
+			window.localStorage.setItem(key, JSON.stringify(value))
+		} catch (error) {
+			console.log(error)
+			return initialValue;
+		}
 	}
 
 	return [storedValue, setValue]
