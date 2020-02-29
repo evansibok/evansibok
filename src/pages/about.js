8ee1e620e758cc36e09fd438412d43073b7564d8
Evans Ibok <evansibok@gmail.com>
2020-02-29 19:27:23 +0100

    Add evans image and design skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,31 +25,7 @@ const AboutPage = () => {
       evansImage: imageSharp(
         fluid: {
           src: {
-            eq: "/static/8ab3b947b1252d44d0090d87ae787e60/b17c1/ev.jpg"
-          }
-        }
-      ) {
-        fluid {
-          src
-          originalName
-        }
-      }
-      gatsbyIcon: imageSharp(
-        fluid: {
-          src: {
-            eq: "/static/4a9773549091c227cd2eb82ccd9c5e3a/7e783/gatsby-icon.png"
-          }
-        }
-      ) {
-        fluid {
-          src
-          originalName
-        }
-      }
-      figmaIcon: imageSharp(
-        fluid: {
-          src: {
-            eq: "/static/a786ce194a5c509fc76de9ec7f101529/7e783/figma-icon.png"
+            eq: "/static/7cbe57754e8d5673cb29cde51e6c93d4/b17c1/evans.jpg"
           }
         }
       ) {
@@ -60,12 +36,6 @@ const AboutPage = () => {
       }
     }
   `)
-
-  const gatsbyImage = data.gatsbyIcon.fluid.src
-  const gatsbyImageAlt = data.gatsbyIcon.fluid.originalName
-
-  const figmaImage = data.figmaIcon.fluid.src
-  const figmaImageAlt = data.figmaIcon.fluid.originalName
 
   const evansImage = data.evansImage.fluid.src
   const evansImageAlt = data.evansImage.fluid.originalName
@@ -87,50 +57,24 @@ const AboutPage = () => {
             functional code for websites, I listen to music, watch fantasy
             shows, drink coffee, read a book or write a poem.
           </p>
-          <p>Interests include VR/AR & Machine Learning</p>
-          <div>
+          <p>I find that VR/AR & Machine Learning intrigue me.</p>
+          <div className="skills">
             <p>My skills include:</p>
-            {
-              skills.map(skill => {
-                return <ul>
-                  <li>{skill}</li>
-                </ul>
-              })
-            }
-            {/* <ul>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>LESS</li>
-              <li>SASS</li>
-              <li>JS</li>
-              <li>ReactJS</li>
-              <li>Redux</li>
-              <li>NodeJS</li>
-              <li>ExpressJS</li>
-              <li>Postgres</li>
-              <li>Knex.js</li>
-              <li>GatsbyJS</li>
-              <li>Github</li>
-              <li>Figma</li>
-              <li>Trello</li>
-            </ul> */}
+            <ul>
+              {
+                skills.map((skill, i) => {
+                  return (
+                      <li key={i}>
+                        {skill}
+                      </li>
+                  )
+                })
+              }
+            </ul>
           </div>
         </div>
 
         <div className="rightCon">
-          {/* <HTML5 />
-          <CSS3 />
-          <JS />
-          <ReactJS />
-          <Redux />
-          <SASS />
-          <NodeJS />
-          <ExpressJS />
-          <GraphQLIcon />
-          <img src={gatsbyImage} alt={gatsbyImageAlt} />
-          <Github />
-          <img src={figmaImage} alt={figmaImageAlt} className="fig" />
-          <Trello /> */}
           <img src={evansImage} alt={evansImageAlt} />
         </div>
       </div>
