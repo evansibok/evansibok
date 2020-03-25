a07d0d3b9b1eac882ce2fa4a17d23ec2860232aa
Evans Ibok <evansibok@gmail.com>
2020-03-25 12:13:27 +0100

    chore: add postgres jsx component to project

--- src/components/skills/expressReactSkills.js
+++ src/components/skills/expressReactSkills.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 import CSS3 from "../svgs/css3-component"
 import JS from "../svgs/js-component"
-import ReactJS from "../svgs/react-js-component"
 import Github from "../svgs/github-component"
 import NodeJS from "../svgs/node-js"
 import ExpressJS from "../svgs/express-js"
-
+import PostGres from "../svgs/postgres-component";
 
 const ExpressReactSkills = () => (
   <div className="projectIcons">
@@ -14,6 +13,7 @@ const ExpressReactSkills = () => (
     <JS />
     <NodeJS />
     <ExpressJS />
+    <PostGres />
     <Github />
   </div>
 )
