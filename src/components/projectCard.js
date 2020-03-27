0587147f97b7f7eddc73551bd18a884c12fe7f55
Evans Ibok <evansibok@gmail.com>
2020-03-27 11:21:35 +0100

    chore: add state to toggle the project body content

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProjectCard = ({
   projectTitle,
@@ -12,30 +12,37 @@ const ProjectCard = ({
   web,
   gH
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openProject = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="projectCard">
-      <div className="contentCon">
-        <div className="cardHead">
-          <h4>{projectTitle}</h4>
+      {/* <div className="contentCon"> */}
+      <div className="cardHead" onClick={openProject}>
+        <h4>{projectTitle}</h4>
 
-          <ul>
-            <a
-              href={websiteAnchorHref}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li>{web}</li>
-            </a>
-            <a
-              href={githubAnchorHref}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li>{gH}</li>
-            </a>
-          </ul>
-        </div>
+        <ul>
+          <a
+            href={websiteAnchorHref}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li>{web}</li>
+          </a>
+          <a
+            href={githubAnchorHref}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li>{gH}</li>
+          </a>
+        </ul>
+      </div>
 
+      <div className="cardBodyCon" style={{ display: isOpen ? 'unset' : 'none' }}>
         <div className="cardBody">
           <div className="leftCon">
             <p>
@@ -50,7 +57,9 @@ const ProjectCard = ({
             <img src={cardImageSrc} alt={cardImageAlt} />
           </div>
         </div>
+
       </div>
+      {/* </div> */}
     </div>
   )
 }
